f296afed1993d7c28edd5f9adcfd41465667d823
Laszlo Papp <lpapp@kde.org>
2013-01-16 14:24:25 +0000

    Launch the application by default after the deploying

--- frontends/blackberry/deploy-to-blackberry.py
+++ frontends/blackberry/deploy-to-blackberry.py
@@ -33,7 +33,7 @@ def main():
     args = parser.parse_args()
 
     subprocess.call(["blackberry-nativepackager", "-package", "-target", "bar", "%s.bar" % args.package_name, args.bar_descriptor_xml_path, "-devMode"])
-    subprocess.call(["blackberry-deploy", "-installApp", "-device", args.ipaddress, "-password", args.password, "-package", "%s.bar" % args.package_name])
+    subprocess.call(["blackberry-deploy", "-installApp", "-device", args.ipaddress, "-launchApp", "-password", args.password, "-package", "%s.bar" % args.package_name])
 
     os.remove("%s.bar" % args.package_name)
 
