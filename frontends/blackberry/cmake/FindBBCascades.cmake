b4939b8721d8a57aafdeb17920b3a9914b3f7461
Laszlo Papp <lpapp@kde.org>
2013-01-16 20:40:41 +0000

    Give the responsibility to the BBCascades finding module to find Qt4.8

--- frontends/blackberry/cmake/FindBBCascades.cmake
+++ frontends/blackberry/cmake/FindBBCascades.cmake
@@ -12,6 +12,12 @@
 #  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
 
+if (BBCascades_FIND_REQUIRED)
+    set(_bbCascadesReq "REQUIRED")
+endif (BBCascades_FIND_REQUIRED)
+
+find_package(Qt 4.8 ${_bbCascadesReq})
+
 find_path(BBCASCADES_INCLUDE_DIR bbcascades_global.h
   HINTS
   ENV BBCASCADESDIR
